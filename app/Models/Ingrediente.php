de71d868fad900f925b57d8b06f212ce374a4356
Hugo Hernández Valdez <hugohv10@gmail.com>
2022-07-05 00:57:59 -0500

    FIX(:bug:): relaciones de miraciones, seeder ingredientes y nombres de migraciones

--- app/Models/Ingrediente.php
+++ app/Models/Ingrediente.php
@@ -4,15 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Ingrediente extends Model
 {
+    protected $primaryKey = 'ingrediente_id';
     protected $table = 'ingrediente';
 
     use HasFactory;
 
-    public function ingredientes()
+    public function ingredientes(): BelongsToMany
     {
-        return $this->belongsToMany(Pizza::class);
+        return $this->belongsToMany(Pizza::class, 'pizza_ingrediente', 'fk_ingrediente', 'fk_pizza');
     }
 }
--- app/Models/Pizza.php
+++ app/Models/Pizza.php
@@ -4,15 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Pizza extends Model
 {
+    protected $primaryKey = 'pizza_id';
     protected $table = 'pizza';
 
     use HasFactory;
 
-    public function ingredientes()
+    public function ingredientes(): BelongsToMany
     {
-        return $this->belongsToMany(Ingrediente::class);
+        return $this->belongsToMany(Ingrediente::class, 'pizza_ingrediente', 'fk_pizza', 'fk_ingrediente');
     }
 }
