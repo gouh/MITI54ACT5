cfc03dd3abd34c50edb97d2cf5a291899e8c3d31
Hugo Hernández Valdez <hugohv10@gmail.com>
2022-07-05 01:52:45 -0500

    FIX(:bug:): Permisos, mensajes y variables de entorno

--- run.sh
+++ run.sh
@@ -9,10 +9,14 @@ docker-compose up -d
 # Time to up services
 sleep 10s
 
+echo "Espere por favor, en un momento realizaremos las instalaciones de dependencias."
+
 # Install dependencies
 docker exec -it pizzeria_php bash -c "composer install"
 
 # Run migrations and seeders
 docker exec -it pizzeria_php bash -c "php artisan migrate --seed"
 
-echo "Se finalizo el proceso para correr el proyecto, verificar en http://localhost:8111/pizzas"
+docker exec -it pizzeria_php bash -c "cd /var/www/pizzeria && chmod 777 -R ."
+
+echo "Se finalizo el proceso para correr el proyecto, verificar en http://localhost:8111/pizzas ."
